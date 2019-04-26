06249240218cbcc9451b0048170261bfdeae45df
ouyangwei <1162956081@qq.com>
2019-04-26 16:26:02 +0800

    add

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="home-header" @click="apply()">
+    </div>
   </div>
 </template>
 
@@ -13,6 +15,32 @@ export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      name: '',
+      person: {
+        name: ''
+      }
+    }
+  },
+  method: {
+    apply () {
+      var hello = ''
+      console.log(hello)
+      if (hello === '') {
+        console.log('hello')
+      }
+    }
   }
 }
 </script>
+<style lang="less" scoped>
+.home {
+  &-header {
+    width: 20px;
+    height: 30px;
+    background-color: #000;
+  }
+}
+</style>
